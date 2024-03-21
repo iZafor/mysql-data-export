e6ed640e19d66a560c2263ee7ae012cb768915a1
iZafor <izafor65@gmail.com>
2024-03-21 06:08:40 +0600

    move value parsing logic to RowEx trait

--- src/traits.rs
+++ src/traits.rs
@@ -23,10 +23,43 @@ impl ColumnTypeEx for ColumnType {
 
 pub trait RowEx {
     fn get_secure<T: FromValue>(&self, i: usize) -> Option<T>; 
+
+    fn get_value(&self, i: usize) -> String;
 }
 
 impl RowEx for Row {
     fn get_secure<T: FromValue>(&self, i: usize) -> Option<T> {
         self.get_opt(i).and_then(|v| v.ok())
+    }
+
+    fn get_value(&self, i: usize) -> String {
+        if self.is_empty() {
+            return String::new();
+        }
+
+        let data_type = self.columns().get(i).unwrap().column_type();
+        if data_type.is_boolean_type() {
+            return match self.get_secure::<u8>(i) {
+                Some(v) => format!("{v}"),
+                None => String::new(),
+            };
+        } else if data_type.is_character_type() {
+            return match self.get_secure::<String>(i) {
+                Some(v) => format!("\"{v}\""),
+                None => format!("\"\""),
+            };
+        } else if data_type.is_numeric_type() {
+            return match self.get_secure::<f32>(i) {
+                Some(v) => format!("{:.2}", v),
+                None => String::new(),
+            };
+        } else if data_type.is_date_time_type() {
+            return match self.get_secure::<time::PrimitiveDateTime>(i) {
+                Some(v) => format!("\"{v}\""),
+                None => format!("\"\""),
+            };
+        } else {
+            return String::new();
+        }
     }
 }
